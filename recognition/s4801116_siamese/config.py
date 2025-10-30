2bbef6f7865ce74bf792ec9e2517d2a8076a3db5
FireFly49 <lalitsuresh195@gmail.com>
2025-10-30 19:05:05 +1000

    Added a config file to store all hyperparameters and global constants in a singular file

---
+++ recognition/s4801116_siamese/config.py
@@ -0,0 +1,33 @@
+"""
+config.py
+
+(ADD DESCRIPTION
+
+Author: Lalit Suresh
+Date: 25th October 2025
+
+"""
+import os
+
+# Image pathing and directories
+DATA_ROOT = "data"
+PROCESSED_IMGS_FOLDER = "processed"
+RAW_IMGS_FOLDER = "raw"
+TRAIN_IMGS_FOLDER = "train-image"
+
+METADATA_CSV_PATH = os.path.join(DATA_ROOT, RAW_IMGS_FOLDER, "train-metadata.csv")
+RAW_IMG_DIR = os.path.join(DATA_ROOT, RAW_IMGS_FOLDER, TRAIN_IMGS_FOLDER)
+PARTIONED_IMGS_DIR = os.path.join(DATA_ROOT, PROCESSED_IMGS_FOLDER)
+
+# Dataset parameters
+BATCH_SIZE = 32
+WORKERS = 4
+TRAIN_SPLIT = 0.8
+VAL_SPLIT = 0.2
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+
+BATCH_SIZE = 32
+LEARNING_RATE = 1e-4
+EPOCHS = 20
+EMBEDDING_DIM = 256
